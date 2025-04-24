aca4edb84dafcc467bf0237cc8175ef35b70099c
Matumermu <nogma1989@gmail.com>
2025-04-24 16:41:52 -0300

    teste inicial
    um programa antigo, so para testar o funcionamento do github

---
+++ teste.py
@@ -0,0 +1,17 @@
+from traceback import print_tb
+
+print ('MENU:')
+print ("1 - nome")
+print ('2 - idade')
+print ('3 - pais')
+print ('0 - sair')
+
+
+opcao = input ('digite o comando')
+
+if opcao == '1':
+    nome = input('digite o nome por favor')
+if opcao == '2':
+    idade = input('digite a idade por favor')
+if opcao == '3':
+    pais = input('digite o pais por favor')
